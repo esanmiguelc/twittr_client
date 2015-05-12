85f4ca8254a483a0bee9b03f79e386696f693a58
esanmiguelc <esanmiguel@gmail.com>
2015-05-11 23:02:35 -0500

    updating gitignore

--- lib/twittr/application_controller.rb
+++ lib/twittr/application_controller.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 module Twittr
   class ApplicationController < Sinatra::Base
 
-   #CONSUMER_KEY = YOUR_APPLICATION_KEY
-   #CONSUMER_SECRET = YOUR_APPLICATION_SECRET
+   CONSUMER_KEY = "9B8p9gxtml1eiykSHZO23HZRT"
+   CONSUMER_SECRET = "4y36UYbf2B6rhyrQljXPp8bNKFcoYD6pkddjTJY9LcTueKcohz" 
+
     set :server, 'webrick'
     
     set :views, File.expand_path('../views', __FILE__)
